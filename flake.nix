d58f4244691c918efb0e7cd85defc2edd71d55d8
= <ivar.scholten@protonmail.com>
2022-04-13 22:44:45 +0200

    flake: update lock, cleanup

--- flake.nix
+++ flake.nix
@@ -1,44 +1,49 @@
 {
-  description = "A tui top clone written in C++";
+  description = "A work-in-progresss TUI task manager for Linux written in C++";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    let
-      version = builtins.substring 0 8 self.lastModifiedDate;
+  outputs = { self, nixpkgs }: let
+    version = builtins.substring 0 8 self.lastModifiedDate;
 
-      # All supported systems
-      forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ];
+    # All supported systems
+    forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ];
 
-      # Nixpkgs instantiated for each supported system
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ self.overlay ]; });
-    in {
-      overlay = final: prev: {
-        tuitop = with final; clangStdenv.mkDerivation rec {
-          pname = "tuitop";
-          inherit version;
+    # Nixpkgs instantiated for each supported system
+    nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ self.overlays.default ]; });
+  in {
+    overlays.default = final: prev: {
+      tuitop = with final; clangStdenv.mkDerivation rec {
+        pname = "tuitop";
+        inherit version;
 
-          src = ./.;
+        src = ./.;
 
-          nativeBuildInputs = [
-            cmake
-            pkg-config
-          ];
+        nativeBuildInputs = [
+          cmake
+          pkg-config
+        ];
 
-          buildInputs = [
-            ftxui
-            fmt
-            procps
-          ];
+        buildInputs = [
+          ftxui
+          fmt
+          procps
+        ];
+
+        meta = with lib; {
+          homepage = "https://github.com/IvarWithoutBones/tuitop";
+          description = "A work-in-progresss TUI task manager for Linux written in C++";
+          license = licenses.gpl3Only;
+          platforms = platforms.linux;
         };
       };
-
-      packages = forAllSystems (system: {
-        inherit (nixpkgsFor.${system}) tuitop;
-      });
-
-      defaultPackage = forAllSystems (system: self.packages.${system}.tuitop);
     };
+
+    packages = forAllSystems (system: {
+      inherit (nixpkgsFor.${system}) tuitop;
+      default = (nixpkgsFor.${system}).tuitop;
+    });
+  };
 }
